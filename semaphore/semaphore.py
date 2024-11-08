c882b6a1fbf9dac5f36ff29805a17bf4d1224940
ivanlosarcos <ivanlosarcosbusiness@hotmail.com>
2024-11-08 17:09:47 +0100

    Final version of semaphore code

--- semaphore/semaphore.py
+++ semaphore/semaphore.py
@@ -3,27 +3,48 @@ from time import sleep
 
 button = Button(21)
 
-green = LED(12)
+green = LED(20)
 yellow = LED(16)
-red = LED(20)
+red = LED(12)
 
 buzzer = Buzzer(2)
 
+def regresive_count(time):
+    for i in range(time, 0, -1):
+        if i > 5:  
+            buzzer.beep()
+            sleep(1)
+        else:
+            buzzer.beep()
+            sleep(0.5)
+        print(f"Tiempo restante: {i} segundos")
 
-button.wait_for_press()
-while True:
-    red.on()
-    sleep(1)
-    red.off()
 
-    yellow.on()
-    sleep(1)
-    yellow.off()
+def semaphore():
 
-    green.on()
-    sleep(1)
-    green.off()
+    while True:
+        green.on()
+        button.wait_for_press()
+        buzzer.beep()
 
-    buzzer.beep()
+        regresive_count(10)
 
-    button.wait_for_press() = break
+        green.off()
+        yellow.on()
+        sleep(2)
+        yellow.off()
+
+
+        red.on()
+        buzzer.beep()
+        sleep(2)
+
+        regresive_count(10)
+        
+        red.off()
+        yellow.off()
+        
+        sleep(1)
+        
+
+semaphore()
